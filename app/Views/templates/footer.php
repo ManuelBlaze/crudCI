edca94ec8d3189cd210c6b0827373290f8268cb7
ManuelBlaze <manuel.escobar1217@gmail.com>
2020-07-29 19:13:53 -0500

    Link Github

--- app/Views/templates/footer.php
+++ app/Views/templates/footer.php
@@ -1,5 +1,5 @@
     <footer class = "bg-light text-center py-2 footer">
-        <a style="text-decoration:none" class="text-white" href="https://github.com/ManuelBlaze " target="_blank "><i class="fab fa-github"></i> Manuel Escobar</a> | 2020
+        <a style="text-decoration:none" class="text-white" href="https://github.com/ManuelBlaze/crudCI " target="_blank "><i class="fab fa-github"></i> Manuel Escobar</a> | 2020
     </footer>
 
     </body>
